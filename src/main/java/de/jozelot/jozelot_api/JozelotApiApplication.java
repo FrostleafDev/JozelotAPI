bca086b764e20d291ad6e27b3fd3b663571938a7
jozelot_ <jozelot77@gmail.com>
2026-05-04 10:39:55 +0200

    init: push project

---
+++ src/main/java/de/jozelot/jozelot_api/JozelotApiApplication.java
@@ -0,0 +1,13 @@
+package de.jozelot.jozelot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JozelotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JozelotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/jozelot/jozelot_api/JozelotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jozelot.jozelot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JozelotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
